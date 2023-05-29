a62feecd79fc42c0264b27775d6e07bd656d52bd
viit0r <vituuoliveira1@gmail.com>
2023-05-29 20:24:31 -0300

    Removendo arquivos desnecessários

--- target/classes/db/migration/V1__Create_Table_Pessoa.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `pessoa` (
-  `id_pessoa` bigint NOT NULL AUTO_INCREMENT,
-  `nome` varchar(80) NOT NULL,
-  `sobrenome` varchar(80) NOT NULL,
-  `endereco` varchar(100) NOT NULL,
-  `genero` varchar(9) NOT NULL,
-  PRIMARY KEY (`id_pessoa`)
-);
--- target/classes/db/migration/V2__Populate_Table_Pessoa.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `pessoa` (`id_pessoa`,`endereco`,`genero`,`nome`,`sobrenome`) VALUES
-    (1,'São Paulo','Masculino','Vitor','Porto'),
-    (2,'São Paulo','Feminino','Aurea','Dutra'),
-    (3,'São Paulo','Feminino','Maria','Joaquina');
