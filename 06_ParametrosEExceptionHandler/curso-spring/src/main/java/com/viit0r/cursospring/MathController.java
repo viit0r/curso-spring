4da677e8021fafc856392c305a6a27b8751d1f95
viit0r <vituuoliveira1@gmail.com>
2023-08-01 20:24:22 -0300

    Implementada as outras operações matemáticas

--- 06_ParametrosEExceptionHandler/curso-spring/src/main/java/com/viit0r/cursospring/MathController.java
+++ 06_ParametrosEExceptionHandler/curso-spring/src/main/java/com/viit0r/cursospring/MathController.java
@@ -21,6 +21,55 @@ public class MathController {
         return convertToDouble(numeroUm) + convertToDouble(numeroDois);
     }
 
+    @RequestMapping(value = "/subtrai/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double subtrai(
+            @PathVariable(value = "numeroUm") String numeroUm,
+            @PathVariable(value = "numeroDois") String numeroDois) throws Exception {
+        if(!isNumerico(numeroUm) || !isNumerico(numeroDois)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico");
+        }
+        return convertToDouble(numeroUm) - convertToDouble(numeroDois);
+    }
+
+    @RequestMapping(value = "/multiplica/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double multiplica(
+            @PathVariable(value = "numeroUm") String numeroUm,
+            @PathVariable(value = "numeroDois") String numeroDois) throws Exception {
+        if(!isNumerico(numeroUm) || !isNumerico(numeroDois)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico");
+        }
+        return convertToDouble(numeroUm) * convertToDouble(numeroDois);
+    }
+
+    @RequestMapping(value = "/divide/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double divide(
+            @PathVariable(value = "numeroUm") String numeroUm,
+            @PathVariable(value = "numeroDois") String numeroDois) throws Exception {
+        if(!isNumerico(numeroUm) || !isNumerico(numeroDois)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico");
+        }
+        return convertToDouble(numeroUm) / convertToDouble(numeroDois);
+    }
+
+    @RequestMapping(value = "/media/{numeroUm}/{numeroDois}", method = RequestMethod.GET)
+    public Double media(
+            @PathVariable(value = "numeroUm") String numeroUm,
+            @PathVariable(value = "numeroDois") String numeroDois) throws Exception {
+        if(!isNumerico(numeroUm) || !isNumerico(numeroDois)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico");
+        }
+        return (convertToDouble(numeroUm) + convertToDouble(numeroDois)) / 2;
+    }
+    
+    @RequestMapping(value = "/raiz/{numero}", method = RequestMethod.GET)
+    public Double tiraRaiz(
+            @PathVariable(value = "numero") String numero) throws Exception {
+        if(!isNumerico(numero)) {
+            throw new UnsupportedMathOperationException("Por favor, digite um valor numérico");
+        }
+        return Math.sqrt(convertToDouble(numero));
+    }
+
     private Double convertToDouble(String numero) {
         if(numero == null) return 0D;
 
