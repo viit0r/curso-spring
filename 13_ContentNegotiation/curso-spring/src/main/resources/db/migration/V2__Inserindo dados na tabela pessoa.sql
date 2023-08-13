0b2a4c2d8c9d036c91da3964d973fcac53cece73
viit0r <vituuoliveira1@gmail.com>
2023-08-12 23:19:14 -0300

    Adicionando Content Negotiation para haver retornos JSON e XML na nossa aplicação

---
+++ 13_ContentNegotiation/curso-spring/src/main/resources/db/migration/V2__Inserindo dados na tabela pessoa.sql
@@ -0,0 +1,5 @@
+INSERT INTO `pessoa` VALUES
+    (2,'Aquilino Gonçalves da Silva','Male','Vitor','Porto'),
+    (3,'Aquilino Gonçalves da Silva','Female','Aurea','Dutra'),
+    (6,'Rua Giuseppe Adami','Female','Vilma','Porto'),
+    (7,'Kentucky - US','Male','Muhammad','Ali');
