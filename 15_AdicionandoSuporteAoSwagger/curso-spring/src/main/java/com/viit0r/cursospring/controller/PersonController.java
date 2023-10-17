1600ff09ce216ce5700230440b8df289a4b20dea
viit0r <vituuoliveira1@gmail.com>
2023-10-17 16:51:39 -0300

    Adição de configurações do Swagger no application.yaml e implementação das anotações que geram a documentação da API

--- 15_AdicionandoSuporteAoSwagger/curso-spring/src/main/java/com/viit0r/cursospring/controller/PersonController.java
+++ 15_AdicionandoSuporteAoSwagger/curso-spring/src/main/java/com/viit0r/cursospring/controller/PersonController.java
@@ -2,6 +2,12 @@ package com.viit0r.cursospring.controller;
 
 import com.viit0r.cursospring.dto.v1.PersonDTO;
 import com.viit0r.cursospring.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints para gerenciamento das pessoas do sistema")
 public class PersonController {
 
     @Autowired
@@ -18,12 +25,42 @@ public class PersonController {
 
     @GetMapping(
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    //ANOTAÇÃO DO SWAGGER PARA DOC
+    @Operation(summary = "Retorna todas as pessoas", description = "Retorna todas as pessoas registradas no sistema",
+    tags = {"People"},
+    responses = {
+            @ApiResponse(description = "Sucesso", responseCode = "200", content = {
+                    @Content(mediaType = "application/json",
+                            array = @ArraySchema(
+                                    schema = @Schema(
+                                            implementation = PersonDTO.class
+                                    )
+                            )
+                    )
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
     public List<PersonDTO> findAll() {
         return personService.findAll();
     }
 
     @GetMapping(value = "/{id}",
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    //ANOTAÇÃO DO SWAGGER PARA DOC
+    @Operation(summary = "Retorna uma pessoa", description = "Retorna uma das pessoas registradas no sistema",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
     public PersonDTO findById(@PathVariable(value = "id") Long id) {
         return personService.findById(id);
     }
@@ -31,6 +68,16 @@ public class PersonController {
     @PostMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    //ANOTAÇÃO DO SWAGGER PARA DOC
+    @Operation(summary = "Adiciona uma pessoa", description = "Registra uma nova pessoa no sistema",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
     public PersonDTO create(@RequestBody PersonDTO person) {
         return personService.create(person);
     }
@@ -38,11 +85,32 @@ public class PersonController {
     @PutMapping(
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    //ANOTAÇÃO DO SWAGGER PARA DOC
+    @Operation(summary = "Atualiza uma pessoa", description = "Atualiza uma pessoa existente no sistema",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Atualizado", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = PersonDTO.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
     public PersonDTO update(@RequestBody PersonDTO person) {
         return personService.update(person);
     }
 
     @DeleteMapping(value = "/{id}")
+    //ANOTAÇÃO DO SWAGGER PARA DOC
+    @Operation(summary = "Deleta uma pessoa", description = "Deleta uma pessoa existente no sistema",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         personService.delete(id);
         return ResponseEntity.noContent().build();
