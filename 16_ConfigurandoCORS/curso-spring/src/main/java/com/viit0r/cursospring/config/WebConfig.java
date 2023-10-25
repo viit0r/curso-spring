f7b1057224a0ffdc56fa150a4b3205943e8cd966
viit0r <vituuoliveira1@gmail.com>
2023-10-25 11:24:02 -0300

    Aprendendo a implementar CORS de forma global (no arquivo de configuração da aplicação)

--- 16_ConfigurandoCORS/curso-spring/src/main/java/com/viit0r/cursospring/config/WebConfig.java
+++ 16_ConfigurandoCORS/curso-spring/src/main/java/com/viit0r/cursospring/config/WebConfig.java
@@ -1,12 +1,29 @@
 package com.viit0r.cursospring.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.origins:defaultValue}")
+    private String corsOrigins = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        String[] originsPermitidas = corsOrigins.split(",");
+
+        registry.addMapping("/**")
+                //.allowedMethods("GET", "POST", "PUT", "DELETE"); Podemos especificar qual método queremos
+                .allowedMethods("*")
+                .allowedOrigins(originsPermitidas)
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
